a7a114e276d3985b51ee434193cb5d68f5f89ae1
Mikhail <glotov.mikhail@mail.ru>
2018-03-04 19:33:11 +0300

    pop not working

--- hw1/src/Block2.hs
+++ hw1/src/Block2.hs
@@ -1,10 +1,10 @@
 module Block2 where
 
 import Data.List
-import System.Random (newStdGen, randomRs)
+--import System.Random (newStdGen, randomRs)
 
-randomIntList :: Int -> Int -> Int -> IO [Int]
-randomIntList n from to = take n . randomRs (from, to) <$> newStdGen
+-- randomIntList :: Int -> Int -> Int -> IO [Int]
+-- randomIntList n from to = take n . randomRs (from, to) <$> newStdGen
 
 merge :: [Int] -> [Int] -> [Int]
 merge [] y = y
@@ -15,4 +15,12 @@ mergeSort :: [Int] -> [Int]
 mergeSort [] = []
 mergeSort [a] = [a]
 mergeSort x = let half = div (length x) 2 in merge (mergeSort (take half x)) (mergeSort (drop half x))
+
+pop :: Int -> [a] -> a
+pop i list = fst (remove i list)
+
+remove :: Int -> [a] -> (Maybe a, [a])
+remove _ [] = (Nothing, [])
+remove 0 (x:xs) = (Just x, xs)
+remove i (x:xs) = (fst rm, x : snd rm) where rm = remove (i - 1) xs
 
